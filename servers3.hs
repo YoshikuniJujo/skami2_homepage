1c9a15e543acc9e327d9f9aa40a78766d029b3cd
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-10-19 08:35:40 +0000

    refactoring server3.hs

--- servers3.hs
+++ servers3.hs
@@ -1,22 +1,27 @@
 {-# LANGUAGE OverloadedStrings, PackageImports #-}
 
-import Control.Applicative
-import "monads-tf" Control.Monad.State
-import Control.Concurrent
-import Data.Maybe
-import Data.List
-import Data.HandleLike
-import Data.Pipe
-import Data.Pipe.List
-import Data.Time
-import System.IO
-import Numeric
-import Network
-import Network.PeyoTLS.Server
-import Network.PeyoTLS.ReadFile
-import Network.TigHTTP.Server
-import Network.TigHTTP.Types
-import "crypto-random" Crypto.Random
+import Control.Applicative ((<$>), (<*>))
+import "monads-tf" Control.Monad.State (
+	MonadIO, liftIO, forever, void, StateT(..), runStateT )
+import Control.Concurrent (forkIO)
+import Data.Maybe (fromMaybe, maybeToList, listToMaybe)
+import Data.List (isPrefixOf)
+import Data.HandleLike (hlClose)
+import Data.Pipe (Pipe, runPipe, await, (=$=))
+import Data.Pipe.List (toList)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Data.Time (getZonedTime)
+import System.IO (Handle)
+import Numeric (showHex)
+import Network (PortID(..), listenOn, accept)
+import Network.PeyoTLS.Server (PeyotlsM, PeyotlsHandle, TlsHandle, run, open)
+import Network.PeyoTLS.ReadFile (readKey, readCertificateChain)
+import Network.TigHTTP.Server (getRequest, putResponse, response, requestBody)
+import Network.TigHTTP.Types (
+	Request(..), Get(..), Post(..),
+	Response(..), ContentType(..), Type(..), Subtype(..), SetCookie(..) )
+import "crypto-random" Crypto.Random (
+	SystemRNG, createEntropyPool, cprgCreate, cprgFork )
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
@@ -24,21 +29,16 @@ import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.UTF8 as BSU
 import qualified Crypto.Hash.SHA256 as SHA256
 
-import Data.IORef
-import UUID4
+import UUID4 (UUID4, newGen, mkUUID4)
 
 main :: IO ()
 main = do
-	rg <- newIORef =<< newGen
-	let uuid4 = do
-		g <- readIORef rg
-		let (u, g') = mkUUID4 g
-		writeIORef rg g'
-		return u
-	rssn <- newIORef []
-	k <- readKey "2014_private_de.key"
-	c <- readCertificateChain ["2014_cert.pem"]
-	g0 <- cprgCreate <$> createEntropyPool :: IO SystemRNG
+	(k, c, g0, rssn, rg) <- (,,,,)
+		<$> readKey "2014_private_de.key"
+		<*> readCertificateChain ["2014_cert.pem"]
+		<*> (cprgCreate <$> createEntropyPool :: IO SystemRNG)
+		<*> newIORef []
+		<*> (newIORef =<< newGen)
 	soc <- listenOn $ PortNumber 443
 	void . (`runStateT` g0) . forever $ do
 		(h, _, _) <- liftIO $ accept soc
@@ -47,66 +47,72 @@ main = do
 			t <- open h ["TLS_RSA_WITH_AES_128_CBC_SHA"] [(k, c)]
 				Nothing
 			r <- getRequest t
-			resp r rssn t uuid4
+			resp r rssn t rg
 			hlClose t
 
+uuid4IO :: IORef SystemRNG -> IO UUID4
+uuid4IO rg = do
+	g <- readIORef rg
+	let (u, g') = mkUUID4 g
+	writeIORef rg g'
+	return u
+
 resp :: Request PeyotlsHandle -> IORef [(UUID4, String)] -> PeyotlsHandle ->
-	IO UUID4 -> PeyotlsM ()
-resp r rssn t uuid4 =
-			case r of
-				RequestGet _p _v g -> do
-					mun <- liftIO $ getUser rssn (read . BSC.unpack . snd <$> listToMaybe (getCookie g))
-					case mun of
-						Just un -> do
-							as <- liftIO $ (: []) . setUserName un <$> readFile "static/i_know.html"
-							putResponse t
-								((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
-								. LBS.fromChunks $ map BSU.fromString as) {
-									responseContentType = ContentType Text Html []
-									}
-						_ -> do
-							as <- liftIO $ (: []) <$> readFile "static/index.html"
-							putResponse t
-								((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
-								. LBS.fromChunks $ map BSU.fromString as) {
-									responseContentType = ContentType Text Html []
-									}
-				RequestPost _p _v pst -> do
-					liftIO $ do
-						putStrLn "POST"
-						print $ postCookie pst
-					up_ <- runPipe $
-						requestBody r =$= toList
-					let	up = map ((\[n, v] -> (n, v)) . split '=')
-							. split '&'
-							$ maybe "" (concatMap BSC.unpack) up_
-						Just un = lookup "user_name" up
-						Just p = lookup "user_password" up
-					liftIO $ getZonedTime >>= print
-					liftIO $ putStrLn un
-					liftIO $ putStrLn p
-					tbl <- liftIO passwordTable
-					let slt = fromMaybe "" $ getSalt (BSC.pack un) tbl
-					let pw = maybe "" BSC.unpack $ getPw (BSC.pack un) tbl
-					let hs = iterate SHA256.hash 
-						(BSC.pack p `BS.append` slt) !! 10000
-					let pw' = concatMap showH $ BS.unpack hs
-					liftIO $ print pw
-					liftIO $ print pw'
-					liftIO . print $ pw == pw'
-					u <- if pw == pw'
-						then Just <$>
-							liftIO (addUser rssn uuid4 un)
-						else return Nothing
-					pg <- liftIO $ readFile "static/login.html"
-					let msg = flip setUserName pg $ if pw == pw' then un else "Nobody"
-					putResponse t
-						((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
-						. LBS.fromChunks $ map BSU.fromString [msg]) {
-							responseContentType = ContentType Text Html [],
-							responseSetCookie = maybeToList $ cookie <$> u
-							}
-				_ -> error "bad"
+	IORef SystemRNG -> PeyotlsM ()
+resp r rssn t rg = case r of
+	RequestGet _p _v g -> do
+		mun <- liftIO $ getUser rssn (read . BSC.unpack . snd <$> listToMaybe (getCookie g))
+		case mun of
+			Just un -> do
+				as <- liftIO $ (: []) . setUserName un <$> readFile "static/i_know.html"
+				putResponse t
+					((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
+					. LBS.fromChunks $ map BSU.fromString as) {
+						responseContentType = ContentType Text Html []
+						}
+			_ -> do
+				as <- liftIO $ (: []) <$> readFile "static/index.html"
+				putResponse t
+					((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
+					. LBS.fromChunks $ map BSU.fromString as) {
+						responseContentType = ContentType Text Html []
+						}
+	RequestPost _p _v pst -> do
+		liftIO $ do
+			putStrLn "POST"
+			print $ postCookie pst
+		up_ <- runPipe $
+			requestBody r =$= toList
+		let	up = map ((\[n, v] -> (n, v)) . split '=')
+				. split '&'
+				$ maybe "" (concatMap BSC.unpack) up_
+			Just un = lookup "user_name" up
+			Just p = lookup "user_password" up
+		liftIO $ getZonedTime >>= print
+		liftIO $ putStrLn un
+		liftIO $ putStrLn p
+		tbl <- liftIO passwordTable
+		let slt = fromMaybe "" $ getSalt (BSC.pack un) tbl
+		let pw = maybe "" BSC.unpack $ getPw (BSC.pack un) tbl
+		let hs = iterate SHA256.hash 
+			(BSC.pack p `BS.append` slt) !! 10000
+		let pw' = concatMap showH $ BS.unpack hs
+		liftIO $ print pw
+		liftIO $ print pw'
+		liftIO . print $ pw == pw'
+		u <- if pw == pw'
+			then Just <$>
+				liftIO (addUser rssn (uuid4IO rg) un)
+			else return Nothing
+		pg <- liftIO $ readFile "static/login.html"
+		let msg = flip setUserName pg $ if pw == pw' then un else "Nobody"
+		putResponse t
+			((response :: LBS.ByteString -> Response Pipe (TlsHandle Handle SystemRNG))
+			. LBS.fromChunks $ map BSU.fromString [msg]) {
+				responseContentType = ContentType Text Html [],
+				responseSetCookie = maybeToList $ cookie <$> u
+				}
+	_ -> error "bad"
 
 printP :: MonadIO m => Pipe BSC.ByteString () m ()
 printP = await >>= maybe (return ()) (\s -> liftIO (BSC.putStr s) >> printP)
